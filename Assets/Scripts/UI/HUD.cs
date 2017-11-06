c2bf750449af59d6ebcb62fbcd2695bc84f67644
Gelo123321 <Gelo123321@hotmail.com>
2017-11-06 18:39:35 +0200

    Update HUD

--- Assets/Scripts/UI/HUD.cs
+++ Assets/Scripts/UI/HUD.cs
@@ -1,16 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HUD : MonoBehaviour {
 
+	public Button buttonStart;
 	// Use this for initialization
 	void Start () {
-		
+		InvokeRepeating("dff", 10, 3);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
+	}
+
+	void dff()
+	{
+
 	}
 }
