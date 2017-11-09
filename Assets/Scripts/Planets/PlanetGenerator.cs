852c0989d11dd8f24ad9d859cd0c19d0ef6a4531
Gelo123321 <Gelo123321@hotmail.com>
2017-11-09 19:25:41 +0200

    update generator

--- Assets/Scripts/Planets/PlanetGenerator.cs
+++ Assets/Scripts/Planets/PlanetGenerator.cs
@@ -3,65 +3,83 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlanetGenerator {
-	public static int sizeX = 1024;
-	public static int sizeY = 256;
-	public int [,] data;
-	private int [] layerHeight;
-	private int layersSize = 10;
+	public int seed;
+	public static int width = 1024;
+	public static int height = 256;
 
-	void SetLayers()
-	{
-		layerHeight = new int[layersSize];
-		int k = sizeY / layersSize;
-		for(int i = 0; i < layersSize; i++)
-		{
-			layerHeight[i] = k * (i + 1);
-		}
-	}
+	public float scale = 16f;
+	public float smoothness = 2f;
+
+	public int [,] data;
 
 	public void Run()
 	{
-		SetLayers();
+		seed = Random.Range(0, 999);
+		data = new int[width, height];
 
-		GenerateShell();
 		GenerateTerrain();
-		GenerateMines();
+		GenerateMines(-1);
 	}
-	void GenerateShell()
-	{
-		data = new int[sizeX, sizeY];
-		for(int i = 0; i < sizeX; i++)
-			for(int j = 0; j < sizeY; j++)
-				data[i, j] = -1;
-	}
+
 	void GenerateTerrain()
 	{
-		Debug.Log(GetNoise(0, 1));
-		Debug.Log(GetNoise(0, 2));
-		Debug.Log(GetNoise(0, 3));
-		Debug.Log(GetNoise(0, 4));
-
-		
-		for(int i = 0; i < sizeX; i++)
+		for(int i = 0; i < width; i++)
 		{
-			for(int j = 0; j < sizeY; j++)
+			int noise = GetNoise(width, i / 4);
+			for(int j = height - noise; j < height; j++)
 			{
-				int height = GetNoise((float)j / 32, (float)i / 32);
-				for(int k = 0; k < sizeY - height; k++)
-				{
-					data[i, k] = 0;
-				}
+				if(j > height - 16)
+					data[i, j] = -1;
+			}
+			noise = GetNoise(i, i / 4);
+			for(int j = 0; j < height - noise; j++)
+			{	
+				if(j > height - noise - 32)
+					data[i, j] = 0;	
+				else
+					data[i, j] = 1;	
 			}
 		}
 	}
-	void GenerateMines()
-	{
 
-	}
-	int GetNoise(float x, float y)
+	void GenerateMines(int tile, int numMines = 128, int mMinSize = 4, int mMaxSize = 256)
 	{
-		float scale = 64f;
-		float noise = Mathf.PerlinNoise(1.0f / (x + 1), 1.0f / (y + 1)); 
-		return (int)(noise * scale);
+		int holeSize = 2;
+		for(int i = 0; i < numMines; i++)
+		{
+			int mSize = Random.Range(mMinSize, mMaxSize);
+			int xpos = Random.Range(0, width);
+			int ypos = Random.Range(0, height);
+			for(int j = 0; j < mSize; j++)
+			{
+				for(int x = -holeSize; x <= holeSize; x++)
+				{
+					for(int y = -holeSize; y <= holeSize; y++)
+					{
+						if(!(x == 0 && y == 0))
+						{
+							Vector2 tilePos = new Vector2(xpos + x, ypos + y);
+							if(tilePos.x > 0 && tilePos.x < width && tilePos.y > 0 && tilePos.y < height)
+								data[(int)tilePos.x, (int)tilePos.y] = (int)tile;
+						}
+					}
+				}
+
+				xpos += Random.Range(-1, 2);
+				ypos += Random.Range(-1, 2);
+
+				// TODO verify.
+			}
+		}
+	}
+
+	int GetNoise(int x, int y)
+	{
+		float xCoord = (float)x / width * scale + seed;
+		float yCoord = (float)y / height * scale + seed;
+
+		float noise = Mathf.PerlinNoise(xCoord * smoothness, yCoord * smoothness); 
+
+		return Mathf.RoundToInt(noise * scale);
 	}
 }
